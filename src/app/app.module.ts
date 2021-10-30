63f78018893c183a9631253527611211c948f549
WojciechSalapatek <wojciech.salapatek@gmail.com>
2021-10-30 20:22:55 +0200

    inti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SummaryTableComponent } from './summary-table/summary-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryTableComponent
   ],
   imports: [
     BrowserModule
